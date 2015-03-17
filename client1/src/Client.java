5609c210f9ab53d13ea8dbb170afe8b651c9de5a
Katherine <katherine@macs-MacBook-Pro-2.local>
2015-03-17 15:25:32 +0800

    test

---
+++ client1/src/Client.java
@@ -0,0 +1,113 @@
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+
+public class Client implements Runnable{
+
+	private static Socket socket;
+	private String address = "140.112.18.219"; 
+	private int port = 8010;
+	private static InetSocketAddress isa;
+	private DataOutputStream os;
+	private DataInputStream is;
+	private static Thread thread;
+	
+	Client() {
+		socket = new Socket();
+		isa = new InetSocketAddress(this.address, this.port);		
+		connect();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			while (true) {
+				String TransferLine = is.readUTF();
+				System.out.println("Recv: " + TransferLine);
+				
+				parseMsg(TransferLine);
+			}
+		}
+		catch (Exception e) {
+			interrupt();
+		}
+	}
+	
+	private void parseMsg(String transferLine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void interrupt() {
+		// TODO Auto-generated method stub
+		System.out.println("interrupt!!!");
+		
+	}
+
+	public void connect() {
+		
+		try {
+			socket.connect(isa, 10000); //timeout
+			//BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+			//BufferedInputStream in = new BufferedInputStream(socket.getInputStream());           
+            
+            System.out.println("資料庫連線成功");
+            //sendName();
+            
+            os = new DataOutputStream(socket.getOutputStream());
+            is = new DataInputStream(socket.getInputStream());
+            this.os = new DataOutputStream(os);
+            this.is = new DataInputStream(is);
+            
+            // 送出字串
+            send("hi");
+            thread = new Thread(this);
+            thread.start(); // run()
+            
+		}
+		catch(Exception e){
+			System.out.println("Socket連線有問題 !");
+            System.out.println("IOException :" + e.toString());
+			
+		}
+	}
+	
+	synchronized private void sendName() throws IOException {
+		// TODO Auto-generated method stub
+		DataOutputStream o = new DataOutputStream(socket.getOutputStream());
+		DataInputStream i = new DataInputStream(socket.getInputStream());
+		
+		String msg = "QAQ";
+		
+		try {
+			os.writeUTF(msg);
+			System.out.println("send: " + msg);
+		}
+		catch (Exception e) {
+			interrupt();
+		}
+		
+		
+	}
+
+	private void send(String msg) {
+		// TODO Auto-generated method stub
+		
+		try {
+			os.writeUTF(msg);
+			System.out.println("send: " + msg);
+		}
+		catch (Exception e) {
+			interrupt();
+		}
+		
+	}
+
+	public static void main(String args[]) {
+        new Client();
+        //connect();
+    }
+}
