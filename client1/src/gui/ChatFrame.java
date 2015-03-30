8a87a43751cf33703ee9d54330fae02f7ca1ef29
starlittle <b00901083@ntu.edu.tw>
2015-03-30 17:41:29 +0800

    connect window with default

--- client1/src/gui/ChatFrame.java
+++ client1/src/gui/ChatFrame.java
@@ -8,15 +8,19 @@ import java.awt.event.ActionListener;
 import java.util.Vector;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.SimpleAttributeSet;
 
 import client.Client;
+//import gui.ChatTab;
 
 
 public class ChatFrame extends JFrame{
 
 	private Client ClientObject;
-	private ChatTab GuiObject;
-	public  Vector<String> userList;
+	//private ChatTab GuiObject;
+	public  Vector<String> ouserList;
 	private Vector<String> roomList;
 	private int action; // the motion
 	private int whisID;
@@ -24,7 +28,7 @@ public class ChatFrame extends JFrame{
 	private int iuID;
 	
 	//
-	private JFrame temp = new JFrame();
+	//private JFrame temp = new JFrame();
 	//public JTextField input; //textpane
 	//public JTextArea output; 
 	public JTextArea users;
@@ -36,11 +40,12 @@ public class ChatFrame extends JFrame{
 	} */
 	
 	public ChatFrame( Client co) {
-		//initComponents();
+		initComponents();
 		
 		ClientObject = co;
-		GuiObject = new ChatTab();
-		userList = new Vector<String>();
+		//GuiObject = new ChatTab(co);
+		//GuiObject.setVisible(true);
+		ouserList = new Vector<String>();
 		roomList = new Vector<String>();
 		action = 0;
 		roomID = 0;
@@ -49,18 +54,19 @@ public class ChatFrame extends JFrame{
 	public void printonGUI (String msg) {
 		//output.append(msg + "\n"); 
 		//System.out.println("get message " + msg);
-		GuiObject.set_Text(msg);
+		//GuiObject.set_Text(msg);
+		set_Text(msg);
 	}
 	
 	public void userList(String name, int id) {
 		System.out.println("add in user list!");
-		userList.add(id,name);	
+		ouserList.add(id,name);	
 		//users.append(name + "\n");
 	}
 	
 	public void addUser(String name, int id) {
 		System.out.println(name + " " + id);
-		userList.add(id,name);
+		ouserList.add(id,name);
 		//users.append(name + "\n");
 		//output.append(name + " joined! \n");
 	}
@@ -68,7 +74,7 @@ public class ChatFrame extends JFrame{
 		roomList.add(roomID);
 	}
 
-	private void prepareMsg(String msg) {
+	public void prepareMsg(String msg) {
 		
 		if (action == 1) { //whisper
 			ClientObject.sendWhis(whisID,roomID,msg);
@@ -85,6 +91,131 @@ public class ChatFrame extends JFrame{
 		ClientObject.sendall(msg);
 	}
 
+	 private void initComponents() {
+
+	        pPanel = new javax.swing.JPanel();
+	        textField = new javax.swing.JTextField();
+	        jTabbedPane1 = new javax.swing.JTabbedPane();
+	        jScrollPane1 = new javax.swing.JScrollPane();
+	        textPanel = new javax.swing.JTextPane();
+	        jScrollPane2 = new javax.swing.JScrollPane();
+	        userList = new javax.swing.JList();
+	        Eicon = new javax.swing.JButton();
+	        toolBar = new javax.swing.JToolBar();
+
+	        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+	        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+	        javax.swing.GroupLayout pPanelLayout = new javax.swing.GroupLayout(pPanel);
+	        pPanel.setLayout(pPanelLayout);
+	        pPanelLayout.setHorizontalGroup(
+	            pPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	            .addGap(0, 0, Short.MAX_VALUE)
+	        );
+	        pPanelLayout.setVerticalGroup(
+	            pPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	            .addGap(0, 143, Short.MAX_VALUE)
+	        );
+
+	        textField.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+	        textField.addActionListener(new java.awt.event.ActionListener() {
+	            public void actionPerformed(java.awt.event.ActionEvent evt) {
+	                textFieldActionPerformed(evt);
+	            }
+	        });
+
+	        jScrollPane1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+
+	        textPanel.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+	        jScrollPane1.setViewportView(textPanel);
+
+	        jTabbedPane1.addTab("tab1", jScrollPane1);
+
+	        userList.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+	        userList.setForeground(new java.awt.Color(255, 255, 255));
+	        jScrollPane2.setViewportView(userList);
+
+	        Eicon.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+	        Eicon.setText("jButton1");
+
+	        toolBar.setRollover(true);
+
+	        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+	        getContentPane().setLayout(layout);
+	        layout.setHorizontalGroup(
+	            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	            .addGroup(layout.createSequentialGroup()
+	                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	                    .addComponent(pPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addContainerGap()
+	                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	                            .addGroup(layout.createSequentialGroup()
+	                                .addComponent(textField)
+	                                .addGap(50, 50, 50)
+	                                .addComponent(Eicon)
+	                                .addGap(46, 46, 46))
+	                            .addGroup(layout.createSequentialGroup()
+	                                .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 376, javax.swing.GroupLayout.PREFERRED_SIZE)
+	                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+	                                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 169, Short.MAX_VALUE)))))
+	                .addContainerGap())
+	            .addGroup(layout.createSequentialGroup()
+	                .addContainerGap()
+	                .addComponent(toolBar, javax.swing.GroupLayout.PREFERRED_SIZE, 376, javax.swing.GroupLayout.PREFERRED_SIZE)
+	                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+	        );
+	        layout.setVerticalGroup(
+	            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	            .addGroup(layout.createSequentialGroup()
+	                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addComponent(pPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+	                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+	                        .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 365, javax.swing.GroupLayout.PREFERRED_SIZE))
+	                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+	                        .addGap(174, 174, 174)
+	                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 344, javax.swing.GroupLayout.PREFERRED_SIZE)))
+	                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+	                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+	                    .addComponent(textField, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
+	                    .addComponent(Eicon))
+	                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+	                .addComponent(toolBar, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+	        );
+	        //setVisible(true);
+	        pack();
+	    }// </editor-fold>//GEN-END:initComponents
+	 
+	 private void textFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textFieldActionPerformed
+	     message = textField.getText();
+	     textField.setText(null);
+	     prepareMsg(message);
+	    }//GEN-LAST:event_textFieldActionPerformed
+	 
+	    public void set_Text(String mg){
+	        SimpleAttributeSet attrset=new SimpleAttributeSet();
+	         Document docs=textPanel.getDocument();
+	         
+	          try{
+	                docs.insertString(docs.getLength(),mg+"\n",attrset); 	    	
+	             }
+	          catch(BadLocationException ble){
+	            System.out.println("BadLocationException:"+ble);	
+	         }	
+	    }
+	 
+	    // Variables declaration - do not modify//GEN-BEGIN:variables
+	    private javax.swing.JButton Eicon;
+	    private javax.swing.JScrollPane jScrollPane1;
+	    private javax.swing.JScrollPane jScrollPane2;
+	    private javax.swing.JTabbedPane jTabbedPane1;
+	    private javax.swing.JPanel pPanel;
+	    private javax.swing.JTextField textField;
+	    private javax.swing.JTextPane textPanel;
+	    private javax.swing.JToolBar toolBar;
+	    private javax.swing.JList userList;
+	    // End of variables declaration//GEN-END:variables
 /*	private void initComponents() {
 		// TODO Auto-generated method stub
 		
--- client1/src/gui/ConnectWindow.java
+++ client1/src/gui/ConnectWindow.java
@@ -1,5 +1,195 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package gui;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
-public class ConnectWindow {
+/**
+ *
+ * @author HP
+ */
+public class ConnectWindow extends javax.swing.JDialog {
 
+    /**
+     * Creates new form ConnectWindow
+     */
+    public int port;
+    public String IP;
+    public String name;
+    public ConnectWindow(ChatFrame f) {
+        super(f);
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pic = new javax.swing.JPanel();
+        input_port = new javax.swing.JTextField();
+        portLabel = new javax.swing.JLabel();
+        ipLabel = new javax.swing.JLabel();
+        nameLabel = new javax.swing.JLabel();
+        input_name = new javax.swing.JTextField();
+        goButton = new javax.swing.JButton();
+        name_remind = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        input_IP = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
+        setModal(true);
+        
+        javax.swing.GroupLayout picLayout = new javax.swing.GroupLayout(pic);
+        pic.setLayout(picLayout);
+        picLayout.setHorizontalGroup(
+            picLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 183, Short.MAX_VALUE)
+        );
+        picLayout.setVerticalGroup(
+            picLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 170, Short.MAX_VALUE)
+        );
+
+        input_port.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+        input_port.setText("8010");
+
+        portLabel.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+        portLabel.setText("port");
+
+        ipLabel.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+        ipLabel.setText("IP");
+
+        nameLabel.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+        nameLabel.setText("Name");
+
+        input_name.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+
+        goButton.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+        goButton.setText("GO!");
+        goButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                goButtonActionPerformed(evt);
+            }
+        });
+
+        name_remind.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+
+        jButton2.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+        jButton2.setText("Cancel");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        input_IP.setFont(new java.awt.Font("微軟正黑體", 0, 12)); // NOI18N
+        input_IP.setText("140.112.18.219");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(name_remind, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 25, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(goButton)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jButton2))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(85, 85, 85)
+                        .addComponent(nameLabel))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(input_IP)
+                            .addComponent(input_name)
+                            .addComponent(input_port, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                        .addGap(86, 86, 86)
+                                        .addComponent(ipLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                        .addGap(77, 77, 77)
+                                        .addComponent(portLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addGap(0, 0, Short.MAX_VALUE)))))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(pic, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(pic, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(ipLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(input_IP, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(portLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(3, 3, 3)
+                .addComponent(input_port, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(nameLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(input_name, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(name_remind, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(goButton)
+                    .addComponent(jButton2))
+                .addGap(41, 41, 41))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void goButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_goButtonActionPerformed
+        IP = input_IP.getText();
+        String port_text = input_port.getText();
+        port = new java.lang.Integer(port_text).intValue();
+
+        if( input_name.getText().equals("") ) {
+            JOptionPane.showMessageDialog(this, "請輸入名稱", "", JOptionPane.ERROR_MESSAGE);
+        } else if( input_name.getText().contains(" ") ) {
+            JOptionPane.showMessageDialog(this, "名稱請不要有空白", "", JOptionPane.ERROR_MESSAGE);
+            input_name.setText("");
+        } else {
+            name = input_name.getText();
+            setVisible(false);
+       
+    }//GEN-LAST:event_goButtonActionPerformed
+    }
+    
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton goButton;
+    private javax.swing.JTextField input_IP;
+    private javax.swing.JTextField input_name;
+    private javax.swing.JTextField input_port;
+    private javax.swing.JLabel ipLabel;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel nameLabel;
+    private javax.swing.JLabel name_remind;
+    private javax.swing.JPanel pic;
+    private javax.swing.JLabel portLabel;
+    // End of variables declaration//GEN-END:variables
 }
